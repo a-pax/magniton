64a79e6e82dce500aa482f73c90788a6b0b4d0b6
a-pax <amir.paks@gmail.com>
2017-09-15 02:38:37 +0100

    config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = ENV["MAILER_SENDER"]
 
-  config.secret_key = ENV["SECRET_KEY_BASE"]
+  config.secret_key =  '-- secret key --'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
