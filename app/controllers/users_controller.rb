0c5847d506804a02e4aec1f3b9516853e47c49b5
a-pax <amir.paks@gmail.com>
2017-09-15 02:49:17 +0100

    no IP check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :skip_first_page, only: :new
-  before_filter :handle_ip, only: :create
+  # before_filter :handle_ip, only: :create
 
   def new
     @bodyId = 'home'
@@ -68,24 +68,24 @@ class UsersController < ApplicationController
     end
   end
 
-  def handle_ip
-    # Prevent someone from gaming the site by referring themselves.
-    # Presumably, users are doing this from the same device so block
-    # their ip after their ip appears three times in the database.
+  # def handle_ip
+  #   # Prevent someone from gaming the site by referring themselves.
+  #   # Presumably, users are doing this from the same device so block
+  #   # their ip after their ip appears three times in the database.
 
-    address = request.env['HTTP_X_FORWARDED_FOR']
-    return if address.nil?
+  #   address = request.env['HTTP_X_FORWARDED_FOR']
+  #   return if address.nil?
 
-    current_ip = IpAddress.find_by_address(address)
-    if current_ip.nil?
-      current_ip = IpAddress.create(address: address, count: 1)
-    elsif current_ip.count > 2
-      logger.info('IP address has already appeared three times in our records.
-                 Redirecting user back to landing page.')
-      return redirect_to root_path
-    else
-      current_ip.count += 1
-      current_ip.save
-    end
-  end
+  #   current_ip = IpAddress.find_by_address(address)
+  #   if current_ip.nil?
+  #     current_ip = IpAddress.create(address: address, count: 1)
+  #   elsif current_ip.count > 2
+  #     logger.info('IP address has already appeared three times in our records.
+  #                Redirecting user back to landing page.')
+  #     return redirect_to root_path
+  #   else
+  #     current_ip.count += 1
+  #     current_ip.save
+  #   end
+  # end
 end
