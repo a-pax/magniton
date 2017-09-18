6883fbf7ff55732a0fa9d2e75d950859873986c5
a-pax <amir.paks@gmail.com>
2017-09-18 16:33:04 +0100

    amendments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,6 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = 'https://sheltered-harbor-57469.herokuapp.com'
-  config.action_mailer.asset_host = 
 # config.action_mailer.default_url_options = { :host => ENV['DEFAULT_MAILER_HOST'] }
 
   # Log the query plan for queries taking more than this (works
