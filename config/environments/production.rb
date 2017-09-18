9f67e41440d295fdeacb214ff38f6ba7e8cff4e9
a-pax <amir.paks@gmail.com>
2017-09-18 22:45:36 +0100

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Prelaunchr::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
-    domain:               "magnitone.co.uk",
+    domain:               "gmail.com",
     authentication:       "plain",
     enable_starttls_auto: true,
     user_name:            ENV["GMAIL_USERNAME"],
