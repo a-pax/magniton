87be57c6d0bc334030c7225d78f54de274781078
a-pax <amir.paks@gmail.com>
2017-09-23 17:19:21 +0100

    font

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,7 @@ function isiPhone(){
 	   });
 		$('#home .hero .logotext').css('font-family','Verdana');
 		$('#home .hero .london').css('font-family','Verdana');
+		$('#home .form-wrap .submit').css('background','#5ab7b2');
    }	
 
 });
